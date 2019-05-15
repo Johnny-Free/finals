72fbc7aea572b8700572606e2bae0017ff48a6b3
Johnny Free <j.darell.free@gmail.com>
2019-05-15 15:59:26 -0500

    inital commit

---
+++ build/precache-manifest.2836a7432f1902a6ffedb685ca1e330e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6780806c8c3d04be93e254d6430f8fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4a2640ef0455179f86",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bb7c5b6a763ec481d852",
+    "url": "/static/css/main.1fac5eaf.chunk.css"
+  },
+  {
+    "revision": "7d4a2640ef0455179f86",
+    "url": "/static/js/2.d20b36e3.chunk.js"
+  },
+  {
+    "revision": "bb7c5b6a763ec481d852",
+    "url": "/static/js/main.f6a8fe47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d9d1c3fb625ac9858becbff4dd00bcc",
+    "url": "/static/media/movie.8d9d1c3f.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Search from './component/Search'
+import Result from './component/Result'
+import NavBar from './component/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      active : true, //used below in the return to determin which component to rener
+    }
+    this.handleClick = this.handleClick.bind(this); //passed to the search component for the button
+    this.handleClick2 = this.handleClick2.bind(this); //passed to the nav bar to be able to navigate to the home page.
+  }
+
+  // this event handler will toggle the active state from true to false an from false to true depending on current state of active.
+  // this is used mainly to get to the result page from the home page.
+  handleClick(){
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+  //this event handler will toggle the active state to true regardless of the current state of active. Used in the nav bar to return 
+  // to the home page
+  handleClick2(){
+    this.setState({
+      active: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar handleClick2={this.handleClick2}/>
+        {this.state.active && <Search handleClick={this.handleClick}/>} {/* if the state of active is true then search will render */}
+        {!this.state.active && <Result />} {/* if the state of active is false the search will render*/}
+      </div>
+    );
+  }
 }
 
 export default App;
