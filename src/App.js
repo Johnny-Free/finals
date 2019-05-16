120b297fde8f7415e39f38feb053c906f46a20c9
Johnny Free <j.darell.free@gmail.com>
2019-05-15 19:46:51 -0500

    six and the mix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Search from './Search'
-import Result from './Result'
-import NavBar from './NavBar'
+import Search from './Search';
+import Result from './Result';
+import NavBar from './NavBar';
 
 class App extends Component {
   constructor(props){
